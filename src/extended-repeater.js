ec8a44d5cfe9cf9639d8ada6c694aa901b467187
thewebtrident <89080112+thewebtrident@users.noreply.github.com>
2021-10-03 01:44:32 +0300

    feat: add repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Create a repeating string based on the given parameters
  *  
@@ -15,7 +13,38 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  
+  let parameters = {
+    repeatTimes: Number(options.repeatTimes),
+    separator: String(options.separator || '+'),
+    addition: String(options.addition),
+    additionRepeatTimes: Number(options.additionRepeatTimes),
+    additionSeparator: String(options.additionSeparator || '|')
+  }
+
+  let STR = '';
+  let result = [];
+
+  if (options.additionRepeatTimes != 0 && options.additionRepeatTimes != undefined) {
+    for (let i = 1; i <= parameters.additionRepeatTimes; i++) {
+      if (i == 1) STR += str;
+      STR += parameters.addition;
+      if (i == parameters.additionRepeatTimes) break;
+      STR += parameters.additionSeparator;
+    }
+  } else {
+    STR += str;
+  }
+
+  if (options.repeatTimes == undefined && options.additionRepeatTimes == undefined) return STR + options.addition;
+  if (options.repeatTimes == undefined) return STR;
+
+  for (let i = 1; i <= parameters.repeatTimes; i++) {
+    result.push(STR);
+    if (i == parameters.repeatTimes) break;
+    result.push(parameters.separator);
+  }
+
+  return result.join('');
 }
