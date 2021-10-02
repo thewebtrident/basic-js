29c99330a5902f285e2bad3f37229e3cf337ccd0
thewebtrident <89080112+thewebtrident@users.noreply.github.com>
2021-10-03 01:06:39 +0300

    feat: add Count cats solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  if (typeof matrix != 'object' || matrix.length == 0) return 0;
+  let number = 0;
+  let count = 0;
+  let newArr = [];
+  newArr = newArr.concat(...matrix);
+  newArr.forEach(item => {
+    if (item == '^^') count++;
+  });
+  return count;
 }
