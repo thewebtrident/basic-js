494b71c70d0f31af29c94c2d712e2c88e32ac8c8
thewebtrident <89080112+thewebtrident@users.noreply.github.com>
2021-10-03 22:45:19 +0300

    feat: add two-st solutions

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given a string, return its encoding version.
  *
@@ -8,9 +6,23 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * @example
  * For aabbbc should return 2a3bc
- *
+ * aabbccdc
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let count = 1;
+    let value = 1;
+
+    while (str[i] == str[i + value] && str[i] != str[i - 1]) {
+      count++;
+      value++;
+    }
+
+    if (count == 1 && str[i - 1] != str[i]) arr.push(str[i]);
+    if (count > 1) arr.push(`${count + str[i]}`);
+  }
+
+  return arr.join("");
 }
--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  let times = 0;
+  
 }
